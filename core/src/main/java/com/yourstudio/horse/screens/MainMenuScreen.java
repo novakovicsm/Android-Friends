d7733f303b2676ad16adff1f8278caf216aa2205
novakovicsm <novakovicsm@users.noreply.github.com>
2026-02-04 14:26:40 +0000

    mvp version 0.0.1

--- core/src/main/java/com/yourstudio/horse/screens/MainMenuScreen.java
+++ core/src/main/java/com/yourstudio/horse/screens/MainMenuScreen.java
@@ -1,13 +1,16 @@
 package com.yourstudio.horse.screens;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.yourstudio.horse.HorseGame;
 
 public class MainMenuScreen extends ScreenAdapter {
     private final HorseGame game;
     private SpriteBatch batch;
+    private BitmapFont font;
 
     public MainMenuScreen(HorseGame game) {
         this.game = game;
@@ -16,12 +19,16 @@ public class MainMenuScreen extends ScreenAdapter {
     @Override
     public void show() {
         batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.setColor(Color.WHITE);
     }
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0.13f, 0.13f, 0.2f, 1f);
         batch.begin();
+        font.draw(batch, "Android Friends", 48, 96);
+        font.draw(batch, "Main menu placeholder", 48, 64);
         batch.end();
     }
 
@@ -29,6 +36,9 @@ public class MainMenuScreen extends ScreenAdapter {
     public void dispose() {
         if (batch != null) {
             batch.dispose();
+        }
+        if (font != null) {
+            font.dispose();
         }
     }
 }
